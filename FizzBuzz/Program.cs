3c3f7680c409e70fceb42899068e4ab90dc7c21f
Mike Weatherford <mikesweatherford@gmail.com>
2022-09-06 20:47:03 -0400

    2nd commit. changed around output

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,13 +8,13 @@
             {
                 if (i % 3 == 0 && i % 5 == 0)
                 {
-                    Console.WriteLine("Fizz");
+                    Console.WriteLine("FizzBuzz");
                 } else if (i % 3 == 0)
                 {
-                    Console.WriteLine("Buzz");
+                    Console.WriteLine("Fizz");
                 } else if (i % 5 == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
+                    Console.WriteLine("Buzz");
                 } else
                 {
                     Console.WriteLine(i);
